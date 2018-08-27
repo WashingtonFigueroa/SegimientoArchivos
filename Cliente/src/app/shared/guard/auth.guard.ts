3b134ad8c149e62981ebc22ad1de6af0df2d4134
DTE <w.figo.1991@gmail.com>
2018-08-27 16:26:00 -0500

    Auth

--- Cliente/src/app/shared/guard/auth.guard.ts
+++ Cliente/src/app/shared/guard/auth.guard.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
 import { Router } from '@angular/router';
+import {Observable} from 'rxjs';
+import {LoginService} from '../../login.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private router: Router) {}
-
-    canActivate() {
-        if (localStorage.getItem('fileTrackingToken')) {
-            return true;
+    constructor(protected router: Router,
+                protected loginService: LoginService) {}
+    canActivate(next: ActivatedRouteSnapshot,
+                state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        const privilegios = JSON.parse( localStorage.getItem('fileTrackingPrivilegios'));
+        if (localStorage.getItem('token')) {
+            if (state.url === '/dashboard') {
+                return true;
+            }
+            if (privilegios !== null) {
+                for (let i = 0; i < privilegios.length; i++) {
+                    if ('/' + privilegios[i].ruta + '/listar' === state.url)  {
+                        return privilegios[i].activo === 1 ? true : false;
+                    }
+                }
+            }
         }
 
         this.router.navigate(['/login']);
