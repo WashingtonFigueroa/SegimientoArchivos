08f7d691823502bd47b46f51345fcbb6a8372527
FRANCO JESUS MAMANI POZO <mamanipozofrancojesus@gmail.com>
2018-08-30 22:52:48 -0400

    environment localhost y port 8000

--- Cliente/src/environments/environment.prod.ts
+++ Cliente/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    base : 'http://192.168.1.10:8000/api/',
+    base : 'http://localhost:8000/api/',
     ver_archivo: 'ver_archivo/',
     ver_documento: 'ver_documento/'
 };
